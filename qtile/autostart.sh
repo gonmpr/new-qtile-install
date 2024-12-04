20672b08faed8f92639954a411c4ab168855feef
Gonzalo Martin Porro Rosas <121343797+gonmpr@users.noreply.github.com>
2024-12-04 17:39:16 -0300

    Delete qtile directory

--- qtile/autostart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-
-
-
-# para la resolucion 
-xrandr --output Virtual1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output Virtual2 --off --output Virtual3 --off --output Virtual4 --off --output Virtual5 --off --output Virtual6 --off --output Virtual7 --off --output Virtual8 --off
