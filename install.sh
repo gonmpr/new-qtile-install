a797f748f431f4d474b04272f93c93037576a0f0
Gonzalo Martin Porro Rosas <121343797+gonmpr@users.noreply.github.com>
2025-01-19 16:24:11 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo '----------------# INSTALACIONES #----------------' &&
 echo '-------------------------------------------------' &&
 sudo pacman -S xorg-server xorg-xinit xorg-apps xorg-fonts-misc &&
 sudo pacman -S qtile alacritty vim &&
-sudo pacman -S arandr dmenu j4-dmenu-desktop alsa-utils pipewire pipewire-pulse pipewire-alsa wireplumber picom firefox &&
+sudo pacman -S arandr dmenu j4-dmenu-desktop alsa-utils pipewire pipewire-pulse pipewire-alsa wireplumber picom firefox python-psutil &&
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd /home/gonmpr/yay && makepkg -si && cd .. && rm -rf yay/ &&
 sudo pacman -S ly &&
 
